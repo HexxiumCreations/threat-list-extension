2514c3370d30277ac1379ee28b8026086659d2eb
Gilbert Gobbels <gijsh55@gmail.com>
2016-07-01 11:55:58 +0200

    Bug fixing

--- listener.js
+++ listener.js
@@ -10,6 +10,6 @@ function httpGet(theUrl, callback)
 }
 httpGet("https://hexxiumcreations.github.io/threat-list/hexxiumthreatlist.txt", function(res) {
 	var BAD_URLS = res.split("\n")
-	for(index = 7; index < BAD_URLS.length; index++)
+	for(index = 8; index < BAD_URLS.length; index++)
 	{if (BAD_URLS[index].indexOf(window.location.hostname) > -1) {console.log(BAD_URLS[index]); document.write("BAD WEBSITE, GET OUT.")}}
 	})
