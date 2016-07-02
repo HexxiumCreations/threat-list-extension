d34fd3b795d5b67b89c64faa97306d560a50f887
Gilbert Gobbels <gijsh55@gmail.com>
2016-07-02 15:40:45 +0200

    Script loads before page, script now redirects.

--- popup.js
+++ popup.js
@@ -8,12 +8,11 @@ function httpGet(theUrl, callback)
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
 }
-		console.log(chrome.extension.getURL("blocked.html"))
-
 httpGet("https://hexxiumcreations.github.io/threat-list/hexxiumthreatlist.txt", function(res) {
 	var BAD_URLS = res.split("\n")
 	for(index = 8; index < BAD_URLS.length; index++)
-	{if (BAD_URLS[index].replace("||", "").replace("/", "") === window.location.hostname) {console.log(BAD_URLS[index]);
-chrome.runtime.sendMessage({state: "bad"}, function(response) {console.log(response.res)})
-}}		
+	{if (BAD_URLS[index].indexOf(window.location.hostname) > -1) {console.log(BAD_URLS[index]); BAD_FOUND === true}
+	
+}
+if (BAD_FOUND !== true) {document.write("This website is good to go!")}		
 	})
